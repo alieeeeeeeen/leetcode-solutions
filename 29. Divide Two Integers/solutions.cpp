82ab749efc5acbfb8fc4a8cea9175c32d440175f
alieeeeeeeen <elainechen77@icloud.com>
2024-05-28 08:41:30 +0800

    solution 29 add check dividend and divisor

--- 29. Divide Two Integers/solutions.cpp
+++ 29. Divide Two Integers/solutions.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
         long div = labs(dividend), divs = labs(divisor), ans = 0; // get the absolute value
         int sign = min(dividend, divisor) < 0 && max(dividend, divisor) > 0 ? -1 : 1; // get the sign
 
         while(divs <= div) { // if divisor is less than dividend
             long tmp = divs, m = 1;
-            while (tmp << 1 <= div) { // repeat the divisor till it is less than dividend
+            while (tmp << 1 <= div) { // repeat the multiplication till the divisor is less than the dividend
                 tmp <<= 1; // left shift the divisor
                 m <<= 1; // left shift the quotient
             }
