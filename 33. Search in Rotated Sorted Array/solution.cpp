75a20bedb0a6588d8dd99cd6bd63e24ec891417b
alieeeeeeeen <elainechen77@icloud.com>
2024-05-31 10:39:46 +0800

    solution 33

--- 33. Search in Rotated Sorted Array/solution.cpp
+++ 33. Search in Rotated Sorted Array/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1; // get the left and right element
+        int left = 0, right = nums.size() - 1; // get the left and right index
 
         while(left < right) { // binary search base condition
             int mid = left + (right - left) / 2;
             if(nums[mid] == target) return mid;
             if (nums[left] < nums[mid] && target >= nums[left] && target < nums[mid] || (
                 nums[left] > nums[mid] && (target > nums[left] || target > nums[right]) 
-            )) { // if left element is less than mid element, which is simple or
-                 // the left element is larger than the mid which means the mid is one of the element
+            )) { // if left element is less than mid element or
+                 // the left element is larger than the mid which means the mid is one of the elements
                  // from the left subset array
                 right = mid;
             } else {
