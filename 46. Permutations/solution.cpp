ae058ffb713f0822843f389ccebab72fda4fb65a
alieeeeeeeen <elainechen77@icloud.com>
2024-07-03 13:43:35 +0800

    solution 46

---
+++ 46. Permutations/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res; // store the result that remember the past
+        int n = nums.size() - 1; 
+        per(0, n, nums, res);
+        return res;
+    }
+
+    void per(int i, int n, vector<int>& nums, vector<vector<int>>& res) {
+        if(i == n) { // if reaches to the end, then return the result
+            res.push_back(nums);
+            return;
+        }
+
+        for(int j = i; j <= n; j++) { // bottom-up from the base case
+            swap(nums[i], nums[j]); // swap
+            per(i + 1, n, nums, res); // recursive
+            swap(nums[i], nums[j]); // backtracking
+        }
+    }
+};
