aa77d57135ea6be6cd413afbd59c9e5a4bda7532
alieeeeeeeen <elainechen77@icloud.com>
2024-08-19 22:57:28 +0800

    solution 48

--- 48. Rotate Image/solution.cpp
+++ 48. Rotate Image/solution.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
+        int n  = matrix.size();
+
         for (int i = 0; i < n / 2; ++i) { // for each cycle
-            for (int j = i; j < n - i -1; ++j) { // clockwise rotate
+            for (int j = i; j < n - i - 1; ++j) { // clockwise rotate
                 const auto tmp = matrix[i][j];
                 matrix[i][j] = matrix[n - j - 1][i];
                 matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
